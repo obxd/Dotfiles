2c24c0bba78d1b6da4c8b8775db85e03e9895218
Oleg Belo <obxdgithub@gmail.com>
2021-07-21 22:52:09 +0300

    tts root dir

--- nvim/.config/nvim/lua/lsp/js-ts-lsp.lua
+++ nvim/.config/nvim/lua/lsp/js-ts-lsp.lua
@@ -1,4 +1,6 @@
 --
 -- npm install -g typescript typescript-language-server
 --
-require'lspconfig'.tsserver.setup{}
+require'lspconfig'.tsserver.setup{
+ root_dir = function() return vim.loop.cwd() end      -- run lsp for javascript in any directory
+}
